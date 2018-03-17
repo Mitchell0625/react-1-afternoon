86e9011997b12cc25f05af920c6fa296a9e9e734
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-03-17 16:04:40 -0500

    Completed FilterObject.js; adding package-lock.json; updated EvenAndOdd.js

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,8 +1,8 @@
 import React, { Component} from 'react';
 
 class FilterObject extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
 
         this.state = {
             festivals: [
@@ -32,24 +32,36 @@ class FilterObject extends Component{
         }
     }
     handleChange(val){
-        this.setState({ userInput: val });
+        this.setState({ userInput: val});
     }
 
     filterFestivals(){
-        if(this.festivals.hasOwnProperty(this.userInput)){
-            this.state.filteredFestivals.push(this.userInput);
-        this.setState({this.state.filteredFestivals})
-        }
+        let filtered = this.state.festivals.filter((val, i, arr) => this.state.festivals[i].hasOwnProperty(this.state.userInput));
+        this.state.filteredFestivals = filtered;
+        this.setState({filtered: this.state.filteredFestivals})
 
     }
     render(){
         return (
             <div className="puzzleBox filterObjectPB">
-            <h4>Filter Object</h4>
-            <span className="puzzleText">Original:{JSON.stringify(this.state.festivals, null, 10)}</span>
-            <input className= "inputLine" onChange= { (e) => this.handleChange(e.target.value)}></input>
-            <button className="confirmationButton"> Filter</button>
-            <span className="resultsBox filterObjectRB">Filtered:{JSON.stringify(this.state.filteredFestivals, null, 10)}</span>
+                <h4>Filter Object</h4>
+                <span 
+                    className="puzzleText">
+                        Original:{JSON.stringify(this.state.festivals, null, 10)}
+                </span>
+                <input  
+                    className= "inputLine" 
+                    onChange= { (e) => this.handleChange(e.target.value)}>
+                </input>
+                <button 
+                    className="confirmationButton" 
+                    onClick = { () => {this.filterFestivals(this.state.userInput)} }>
+                        Filter
+                </button>
+                <span 
+                    className="resultsBox filterObjectRB">
+                        Filtered:{JSON.stringify(this.state.filteredFestivals, null, 10)}
+                </span>
             </div>
         )
     }
